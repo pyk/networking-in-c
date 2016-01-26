5c3a6f54be7867d935e10cb1768ac6dc03190c4d
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-01-26 23:31:12 +0700

    update split host and port function

--- splithostport.c
+++ splithostport.c
@@ -4,29 +4,18 @@
 
 /* The maximum length of TCP hostname are 253 ASCII character; specified 
  * by RFC 952. */
-#define TCP_HOSTLN 253
-#define TCP_PORTLN 5
+#define TCP_HOSTLN 254
+/* TCP port is stored in 16-bit integer, the maximum value is 65535; 5
+ * digit characters long. */
+#define TCP_PORTLN 6
 
-// bagaimana handle error kalo prototype begini?
-// kemungkinan error adalah format salah
-// atau empty host
-// expected valid case : "host:port"
-// expected invalid case:
-// - "hostport" (invalid format)
-// - ":port" (host not found)
-// - "host:port:extra" (invalid format for port)
-char *tcpsplithost(char hostport[]);
-
-// return error codes. host modified from host.
-// valid host itu apa?
-// max length berapa? terus valid char gimana dll?
-// valid hostname only contain a-zA-Z0-9 and '-'
 int tcpsh(char *host, char addr[]);
 int tcpsp(char *port, int colon_i, char addr[]);
 int tcpsaddr(char *host, char *port, char addr[]);
 
 enum {
-    TCP_INVH = 1
+    TCP_INVH = 1,
+    TCP_INVP
 };
 
 int main()
@@ -34,110 +23,119 @@ int main()
     char host[TCP_HOSTLN];
     char port[TCP_PORTLN];
 
-    char hostport1[] = "localhost:8080";
-    char hostport2[] = "some.example.com:80";
-    char hostport3[] = "*invalidch^r:8080";
-    char hostport4[] = "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz0123456789"
-                       "abcdefijklmnopqrstuvwxyz01234:56789";
-    char hostport6[] = ":8080";
-    char hostport5[] = "localhost8080";
+    /* valid example */
+    char addr1[] = "localhost:8080";
+    char addr2[] = "some.example.com:80";
 
-    assert(tcpsaddr(host, port, hostport1) == 0);
+    assert(tcpsaddr(host, port, addr1) == 0);
+    printf("host: %s; port: %s\n", host, port);
+    assert(tcpsaddr(host, port, addr2) == 0);
     printf("host: %s; port: %s\n", host, port);
 
-    assert(tcpsaddr(host, port, hostport2) == 0);
-    printf("host: %s; port: %s\n", host, port);
+    /* invalid host */
+    char addr3[] = "*invalidch^r:8080";
+    char addr4[] = "invalid8080";
+    char addr5[] = ":8080";
+    char addr6[] = "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz0123456789"
+                       "abcdefijklmnopqrstuvwxyz01234:5678";
 
-    assert(tcpsaddr(host, port, hostport3) == -1);
-    assert(tcpsaddr(host, port, hostport4) == -1);
-    assert(tcpsaddr(host, port, hostport5) == -1);
-    assert(tcpsaddr(host, port, hostport6) == -1);
+    assert(tcpsaddr(host, port, addr3) == TCP_INVH);
+    assert(tcpsaddr(host, port, addr4) == TCP_INVH);
+    assert(tcpsaddr(host, port, addr5) == TCP_INVH);
+    assert(tcpsaddr(host, port, addr6) == TCP_INVH);
+
+
+    /* invalid port */
+    char addr7[] = "host:abc";
+    char addr8[] = "host:123456";
+    char addr9[] = "host:";
+    char addr10[] = "host:8080:extra";
+
+    assert(tcpsaddr(host, port, addr7) == TCP_INVP);
+    assert(tcpsaddr(host, port, addr8) == TCP_INVP);
+    assert(tcpsaddr(host, port, addr9) == TCP_INVP);
+    assert(tcpsaddr(host, port, addr10) == TCP_INVP);
+
 }
 
 /* tcpsh: splits TCP hostname from the host:port address format and write to
- * _host_. 
+ * *host. This function will start reading the hostport[] from the first index 
+ * until the ':' character.
  * 
- * This function do not write more than TCP_HOSTLN bytes (including the
- * terminating null byte ('\0')). The lentgth of _host_ should equal to or 
- * greater than TCP_HOSTLN bytes.
+ * Valid ASCII character for hostname are a-zA-Z0-9 and {'.','-'} as specified 
+ * by RFC 952. The maximum length of hostname are 253 ASCII characters.
  *
- * Upon successful return, this function return the index of ':'. If the 
- * _hostport_ format is invalid, -1 value is returned */
+ * This function do not write more than TCP_HOSTLN bytes (including the
+ * terminating null byte ('\0')) to *host. The size of *host should equal 
+ * to or greater than TCP_HOSTLN bytes.
+ *
+ * Upon successful return, this function return the number or bytes that 
+ * succesfully written to *host. If the hostname is invalid or ':' character
+ * is not found in hostport[], -1 will be returned. */
 int tcpsh(char *host, char hostport[])
 {
-    int valid = 0;
-    /* index of colon from host:port */
-    int colon_i = 0;
+    int nbytes = 0;
 
-    for(int i = 0; hostport[i] != '\0'; i++) {
+    for(int i = 0; hostport[i] != '\0' && hostport[i] != ':'; i++) {
         /* current character */
         char c = hostport[i];
 
-        /* valid ASCII character are a-zA-Z0-9, '.' and '-'; as specified 
-         * by RFC 952 */
-        if(!(isalnum(c) || (c == '-') || (c == '.') || (c == ':'))) break;
+        /* hostname validation */
+        if(!(isalnum(c) || (c == '-') || (c == '.')) || (i >= (TCP_HOSTLN-1))) {
+            host[0] = '\0';
+            return -1;
+        };
 
-        if(c != ':') {
-            /* to make sure not to overflow the buffer _host_ */
-            if(i >= (TCP_HOSTLN-1)) break;
-
-            /* write to _host_ */
-            host[i] = c;
-        } else {
-            /* to handle ":port" invalid format */
-            if(i == 0) break;
-
-            host[i] = '\0';
-            valid = 1;
-            colon_i = i;
-            break;
-        }
+        /* write to *host */
+        host[i] = c;
+        nbytes = i + 1;
     }
 
-    /* empty the invalid host */
-    if(!valid) {
+    if(nbytes == 0 || hostport[nbytes] != ':') {
         host[0] = '\0';
         return -1;
     }
 
-    return colon_i;
+    host[nbytes] = '\0';
+    return nbytes;
 }
 
-/* tcpsp: splits port from the host:port format address and writes to _port_.
- * It starts reading _hostport_ from _colon_i_ index until the null character.
+/* tcpsp: splits port from the host:port format address and write to *port.
+ * This function will start reading the hostport[] from colon_i index until
+ * the null character.
+ * 
+ * Since port is stored in 16-bit interger, valid port are only a 5 digit 
+ * ASCII characters long.
  * 
  * This function do not write more than TCP_PORTLN bytes (including the
- * terminating null byte ('\0')). The lentgth of _port_ should equal to or 
- * greater than TCP_PORTLN bytes.
+ * terminating null byte ('\0')) to *port. The length of *port should 
+ * equal to or greater than TCP_PORTLN bytes.
  * 
- * Upon successful return, this function returns 0. If the 
- * _hostport_ format is invalid, -1 value is returned */
+ * Upon successful return, this function returns the number of bytes that
+ * succesfully written to *port. If the port is invalid, -1 is returned */
 int tcpsp(char *port, int colon_i, char hostport[])
 {
-    int valid = 1;
-    int startindex = colon_i + 1;
     int port_i = 0;
 
-    for(int i = startindex; hostport[i] != '\0'; i++) {
+    /* handle invalid "host:" format */
+    if(hostport[colon_i] != ':') return -1;
+
+    for(int i = colon_i+1; hostport[i] != '\0'; i++) {
         /* current character */
         char c = hostport[i];
 
         /* validate the port */
-        if(!isdigit(c)) {
-            valid = 0;
-            break;
-        }
-        if(i >= ((startindex+TCP_PORTLN)-1)) {
-            valid = 0;
-            break;
+        if(!isdigit(c) || port_i >= (TCP_PORTLN-1)) {
+            port[0] = '\0';
+            return -1;
         }
 
         /* write to _port_ */
@@ -145,25 +143,30 @@ int tcpsp(char *port, int colon_i, char hostport[])
         port_i++;
     }
 
-    if(!valid) {
+    if(port_i == 0) {
         port[0] = '\0';
         return -1;
     }
 
-    return 0;
+    port[port_i] = '\0';
+    return port_i;
 }
 
-/* tcpsaddr: splits host and port from address _addr_ and writes to _host_
- * and _port_.
+/* tcpsaddr: splits host and port from address addr[] and write to *host
+ * and *port.
  *
- * This function combine tcpsh() and tcpsp(). The size of _host_ and _port_
- * should equal to or greater than TCP_HOSTLN and TCP_PORTLN.
+ * This function combine tcpsh() and tcpsp() functions. The size of *host 
+ * and *port should equal to or greater than TCP_HOSTLN and TCP_PORTLN.
  *
- * It returns 0 if success and -1 if _addr_ format is invalid.
+ * It returns 0 if successfully split host and port. 
+ * It returns TCP_INVH if the host is invalid.
+ * It returns TCP_INVP if the port is invalid.
  */
 int tcpsaddr(char *host, char *port, char addr[])
 {
     int colon_i = tcpsh(host, addr);
-    if(colon_i == -1) return -1;
-    return tcpsp(port, colon_i, addr);
+    if(colon_i == -1) return TCP_INVH;
+    int res = tcpsp(port, colon_i, addr);
+    if(res == -1) return TCP_INVP;
+    return 0;
 }
